a748c4c2730bbc379eb50c4d4d097cad62506446
Jose Beneyto <sepen@crux.nu>
2024-12-30 09:57:22 +0100

    Debugging github workflows

--- prepare-files.sh
+++ prepare-files.sh
@@ -6,12 +6,11 @@ readonly CURL_CMD="curl -SL -O"
 
 # we need the version parameter and folder destination
 if [ $# -ne 2 ]; then
-  echo "Usage: $(basename $0) VERSION FOLDER"
+  echo "Usage: $(basename $0) FOLDER"
   exit 0
 fi
 
-readonly VERSION=$1
-readonly FOLDER=$2
+readonly FOLDER=$1
 
 if [ ! -d $FOLDER ]; then
   echo "ERROR: Folder $FOLDER doesn't exists"
@@ -21,12 +20,12 @@ fi
 cd "$FOLDER"
 echo "Preparing files for $FOLDER"
 
-case $VERSION in
-  3.7-updated|3.7-updated-amd64)
+case $FOLDER in
+  3.7-updated-amd64)
     $CURL_CMD https://crux.ninja/updated-iso/crux-3.7-updated.iso
     sudo $ISO2TAR_CMD crux-3.7-updated.iso
     ;;
-  3.7|3.7-amd64)
+  3.7-amd64)
     $CURL_CMD http://ftp.spline.inf.fu-berlin.de/pub/crux/crux-3.7/iso/crux-3.7.iso
     sudo $ISO2TAR_CMD crux-3.7.iso
     ;;
@@ -36,17 +35,17 @@ case $VERSION in
   3.7-armhf)
     $CURL_CMD https://master.dl.sourceforge.net/project/crux-arm/releases/3.7/crux-arm-3.7-rc4.rootfs.tar.xz
     ;;
-  2.6|2.6-amd64)
+  2.6-amd64)
     $CURL_CMD http://ftp.spline.inf.fu-berlin.de/pub/crux/crux-2.6/iso/crux-2.6.iso
-		sudo $ISO2TAR_CMD crux-2.6.iso && \
-		  mkdir tmp && \
-		  tar -C tmp -xf crux-2.6.tar && \
-		  tmp/tools/unsquashfs -d rootfs tmp/crux.squashfs 2>/dev/null && \
-		  cd rootfs && tar cf ../rootfs-2.6.tar * && cd .. && \
-	    rm -r tmp rootfs
+    sudo $ISO2TAR_CMD crux-2.6.iso && \
+      mkdir tmp && \
+      tar -C tmp -xf crux-2.6.tar && \
+      tmp/tools/unsquashfs -d rootfs tmp/crux.squashfs 2>/dev/null && \
+      cd rootfs && tar cf ../rootfs-2.6.tar * && cd .. && \
+      rm -r tmp rootfs
     ;;
   *)
-    echo "Nothing to do ($VERSION)"
+    echo "Nothing to do ($FOLDER)"
     ;;
 esac
 
